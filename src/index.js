abca5c8c97fcd39d285607a041c06d7b139382c3
Ubuntu <ubuntu@ip-172-31-39-185.ap-south-1.compute.internal>
2020-12-20 06:57:56 +0000

    Adding Another File

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Counter from './components/counter'
+
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
+
+
+/*
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,7 +20,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
