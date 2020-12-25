912953f2197185b431b1cb601893f3e217d7b4df
Ubuntu <ubuntu@ip-172-31-39-185.ap-south-1.compute.internal>
2020-12-25 18:23:54 +0000

    Master App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          TANNUKabeerKKK
+          Check Master
         </a>
       </header>
     </div>
